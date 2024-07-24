09c867b089f9b93f1ed41ec2265fc1490ac3dd25
Luong NGUYEN <luongnv89@gmail.com>
2024-07-24 10:18:57 +0200

    use rootReducers in store

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import alertReducer from './alertReducer';
 import sessionReducer from './sessionReducer';
+import authReducer from './authReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   alerts: alertReducer,
-  sessions: sessionReducer
+  sessions: sessionReducer,
+  auth: authReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,20 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import alertReducer from './reducers/alertReducer';
-import sessionReducer from './reducers/sessionReducer';
-import authReducer from './reducers/authReducer';
-import userReducer from './reducers/userReducer';
 import rootSaga from './sagas/rootSaga';
+import rootReducer from './reducers/index';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: {
-    alerts: alertReducer,
-    sessions: sessionReducer,
-    auth: authReducer,
-    user: userReducer,
-  },
+  reducer:rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
 
